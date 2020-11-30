c5c570d33322fab7b703d91982439af7e5feb3f1
GiacomoMeloni <giacomo.meloni.mail@gmail.com>
2020-11-30 21:57:27 +0100

    New class LocationStats created Improved logic

---
+++ src/main/java/io/covidtr/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,41 @@
+package io.covidtr.coronavirustracker.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int leatestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getLeatestTotalCases() {
+        return leatestTotalCases;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLeatestTotalCases(int leatestTotalCases) {
+        this.leatestTotalCases = leatestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", leatestTotalCases=" + leatestTotalCases +
+                '}';
+    }
+}
